c034b174e8bed482428060ae2dd1eba6b72ebbd8
Smith Aitufe <smithaitufe@live.com>
2016-07-05 14:46:32 +0100

    Added student continuous assessment view and controller

---
+++ web/controllers/v1/student_continuous_assessment_controller.ex
@@ -0,0 +1,57 @@
+defmodule PortalApi.V1.StudentContinuousAssessmentController do
+  use PortalApi.Web, :controller
+
+  alias PortalApi.V1.StudentContinuousAssessment
+
+  plug :scrub_params, "student_continuous_assessment" when action in [:create, :update]
+
+  def index(conn, _params) do
+    student_continuous_assessments = Repo.all(StudentContinuousAssessment)
+    render(conn, "index.json", student_continuous_assessments: student_continuous_assessments)
+  end
+
+  def create(conn, %{"student_continuous_assessment" => student_continuous_assessment_params}) do
+    changeset = StudentContinuousAssessment.changeset(%StudentContinuousAssessment{}, student_continuous_assessment_params)
+
+    case Repo.insert(changeset) do
+      {:ok, student_continuous_assessment} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", student_continuous_assessment_path(conn, :show, student_continuous_assessment))
+        |> render("show.json", student_continuous_assessment: student_continuous_assessment)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(PortalApi.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    student_continuous_assessment = Repo.get!(StudentContinuousAssessment, id)
+    render(conn, "show.json", student_continuous_assessment: student_continuous_assessment)
+  end
+
+  def update(conn, %{"id" => id, "student_continuous_assessment" => student_continuous_assessment_params}) do
+    student_continuous_assessment = Repo.get!(StudentContinuousAssessment, id)
+    changeset = StudentContinuousAssessment.changeset(student_continuous_assessment, student_continuous_assessment_params)
+
+    case Repo.update(changeset) do
+      {:ok, student_continuous_assessment} ->
+        render(conn, "show.json", student_continuous_assessment: student_continuous_assessment)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(PortalApi.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    student_continuous_assessment = Repo.get!(StudentContinuousAssessment, id)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(student_continuous_assessment)
+
+    send_resp(conn, :no_content, "")
+  end
+end
---
+++ web/views/v1/student_continuous_assessment_view.ex
@@ -0,0 +1,19 @@
+defmodule PortalApi.V1.StudentContinuousAssessmentView do
+  use PortalApi.Web, :view
+
+  def render("index.json", %{student_continuous_assessments: student_continuous_assessments}) do
+    %{data: render_many(student_continuous_assessments, PortalApi.V1.StudentContinuousAssessmentView, "student_continuous_assessment.json")}
+  end
+
+  def render("show.json", %{student_continuous_assessment: student_continuous_assessment}) do
+    %{data: render_one(student_continuous_assessment, PortalApi.V1.StudentContinuousAssessmentView, "student_continuous_assessment.json")}
+  end
+
+  def render("student_continuous_assessment.json", %{student_continuous_assessment: student_continuous_assessment}) do
+    %{id: student_continuous_assessment.id,
+      course_id: student_continuous_assessment.course_id,
+      staff_id: student_continuous_assessment.staff_id,
+      student_id: student_continuous_assessment.student_id,
+      score: student_continuous_assessment.score}
+  end
+end
