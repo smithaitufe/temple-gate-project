47d22d72fcb5d8cebb8769646cbeb90bbaf939cb
Smith Aitufe <smithaitufe@live.com>
2016-11-01 15:17:44 +0100

    Added prod.secret.exs

---
+++ config/prod.secret.exs
@@ -0,0 +1,14 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :portal_api, PortalApi.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :portal_api, PortalApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),  
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
