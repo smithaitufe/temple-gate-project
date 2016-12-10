59d88255690edf69e44cdea75cc22f8a8f3ebb06
Samuel Smith <smithaitufe@live.com>
2016-12-10 22:55:16 +0100

    Modified recaptcha secret key and site key  to camel case

--- src/settings.js
+++ src/settings.js
@@ -2,8 +2,8 @@ export const brand = "Temple Gate Polytechnic";
 export const restUrl =  "ttp://localhost:4000";
 export const siteUrl =  "http://127.0.0.1:9000";
 export const tokenName = "cupid16_t";
-export const recaptcha_site_key = "6Ld1xSUTAAAAAGlo03e4qEIGpp6UcLlWYfrsbph9";
-export const recaptcha_secret_key = "6Ld1xSUTAAAAADz16z7ybX-reufvQmFNHcPApjY0";
+export const recaptchaSiteKey = "6Ld1xSUTAAAAAGlo03e4qEIGpp6UcLlWYfrsbph9";
+export const recaptchaSecretKey = "6Ld1xSUTAAAAADz16z7ybX-reufvQmFNHcPApjY0";
 export const interswitch = {
     paymentPostUrl: "https://stageserv.interswitchng.com/test_paydirect/pay",
     macKey: 'D3D1D05AFE42AD50818167EAC73C109168A0F108F32645C8B59E897FA930DA44F9230910DAC9E20641823799A107A02068F7BC0F4CC41D2952E249552255710F',
