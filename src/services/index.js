2f5db585dc110a38154062c18d16746de3d3a7a2
Samuel Smith <smithaitufe@live.com>
2016-12-11 13:59:19 +0100

    Added service charge and payment split services

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,3 @@
 export { SessionService } from './session-service';
+export { PaymentService } from './payment-service';
+
--- src/services/payment-service.js
+++ src/services/payment-service.js
@@ -1,44 +1,42 @@
 import { HttpClient } from 'aurelia-fetch-client';
 import CryptoJS from 'crypto-js';
 import { get, post, put } from '../utils';
-import settings from '../settings';
+import { interswitch } from '../settings';
 
 
 export class PaymentService {
 
   constructor(){
-    const { interswitch : { product_id, mac_key, currency, pay_item_id } } = settings;
-    this.product_id = product_id;
-    this.mac_key = mac_key;
+    const { productId, macKey, currency, payItemId } = interswitch;
+    this.productId = productId;
+    this.macKey = macKey;
     this.currency = currency;
-    this.pay_item_id = pay_item_id;
+    this.payItemId = payItemId;
   }
-  get_student_payments(params = null){
+  getPayments(params = null){
     if(params){
-      return get(`/api/v1/student_payments?${params}`)
+      return get(`/api/v1/payments?${params}`)
     }
-    return get(`/api/v1/student_payments`)
+    return get(`/api/v1/payments`)
   }
-  get_student_payment_by_id(student_payment_id){
-    if(student_payment_id){
-      return get(`/api/v1/student_payments/${student_payment_id}`)
+  getPaymentById(id){
+    if(id){
+      return get(`/api/v1/payments/${student_payment_id}`)
     }
     throw new Error("Student payment id not specified")
   }
-
-
-  generate_query_hash(transaction_reference_no){
-    const { interswitch : { product_id, mac_key } } = settings
-    let str = `${transaction_reference_no}${product_id}${mac_key}`;
-    return this.perform_hash(str);
+  generateQueryHash(transactionReferenceNo){
+    const { productId, macKey } = interswitch;
+    let str = `${transactionReferenceNo}${productId}${macKey}`;
+    return this.performHash(str);
   }
-  generate_payment_hash(transaction_reference_no, amount, site_redirect_url){
-    const { interswitch : { product_id, pay_item_id, mac_key } } = settings
-    let str = `${transaction_reference_no}${product_id}${pay_item_id}${amount}${site_redirect_url}${mac_key}`;
-    return this.perform_hash(str);
+  generatePaymentHash(transactionReferenceNo, amount, siteRedirectUrl){
+    const { interswitch : { productId, payItemId, macKey } } = settings
+    let str = `${transactionReferenceNo}${productId}${payItemId}${amount}${siteRedirectUrl}${macKey}`;
+    return performHash(str);
   }
-  query_payment(transaction_reference_no, amount){
-    const { interswitch : { payment_post_url, product_id }} = settings;
+  queryPayment(transactionReferenceNo, amount){
+    const { paymentPostUrl, productId } = interswitch;
     let http = new HttpClient().configure(config => {
       config.withDefaults({
           mode: 'cors',
@@ -46,16 +44,58 @@ export class PaymentService {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin':'*',
-            'Hash': this.generate_query_hash(transaction_reference_no)
+            'Hash': this.generateQueryHash(transactionReferenceNo)
           }
     })
   });
-    let url = payment_post_url + `product_id=${product_id}&transactionreference=${transaction_reference_no}&amount=${amount}`
+    let url = paymentPostUrl + `productid=${productId}&transactionreference=${transactionReferenceNo}&amount=${amount}`
     return http.fetch(url, {method: 'get'}).then(response => response.json())
-  }
-  perform_hash(str){
+  }  
+}
+
+const performHash = (str) => {
     var hash = CryptoJS.SHA512(str);
     var outString =  hash.toString(CryptoJS.enc.Hex)
     return outString;
+}
+
+export class ServiceChargeService{
+  getServiceCharges(params){
+    if(params) return get(`/api/v1/service_charges?${params}}`)
+    return get(`/api/v1/service_charges`);
+  }
+  getServiceChargeById(id){
+    if(!id) throw new Error("Parameter not specified");
+    return get(`/api/v1/service_charges/${id}`);
+  }
+  saveServiceCharge(serviceCharge){
+    new Promise((reject) => {
+      if(!serviceCharge) reject();
+      const { id } = serviceCharge;
+      const data = { service_charge: serviceCharge};
+      if(id) return put(`/api/v1/service_charges/${id}`, data);
+      return post(`api/v1/service_charges`, data);
+    }    
+  }
+
+}
+
+export class PaymentSplitService{
+  getPaymentSplits(params){
+    if(params) return get(`/api/v1/payment_splits?${params}}`)
+    return get(`/api/v1/payment_splits`);
+  }
+  getPaymentSplitById(id){
+    if(!id) throw new Error("Parameter not specified");
+    return get(`/api/v1/payment_splits/${id}`);
+  }
+  savePaymentSplit(paymentSplit){
+    return new Promise(reject => {
+      if(!paymentSplit) reject();
+      const { id } = paymentSplit;
+      const data = { payment_split: paymentSplit};
+      if(id) return put(`/api/v1/payment_splits/${id}`, data);
+      return post(`api/v1/payment_splits`, data);      
+    }    
   }
 }
