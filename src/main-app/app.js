69bf1b315ecc8dfc8d56eb19d338d67aadd5380b
Samuel Smith <smithaitufe@live.com>
2016-12-18 19:06:48 +0100

    Modifications to courses and fee component.

--- src/main-app/app.js
+++ src/main-app/app.js
@@ -2,22 +2,23 @@ import { inject } from 'aurelia-framework';
 import {ValidationController, validateTrigger} from 'aurelia-validation';
 import { RouteMapper } from 'aurelia-route-mapper';
 import { routes as programsRoutes } from './programs/programs-section';
+import { brand } from '../settings';
 
 @inject(RouteMapper)
 export class App {
-  constructor(routeMapper){
-    this.routeMapper = routeMapper;
+  constructor(mapper){
+    this.mapper = mapper;
   }
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = brand;
     config.options.pushState = true;
     config.map(routes);
-    this.routeMapper.map(routes);
+    this.mapper.map(routes);
     this.router = router;
   }
 }
 let routes = [
-  { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome/welcome',      nav: true, title: 'Welcome' },
+  { route: ['/', '/welcome'], name: 'welcome',  moduleId: './welcome/welcome',      nav: true, title: 'Welcome' },
   { route: "/login", name: "login", moduleId: "./login/login", title: "Login", nav: false},
   { route: "/programs", name: "programs-section", moduleId: "./programs/programs-section", title: "Programs", nav: false, settings: { childRoutes: programsRoutes}}
 ]
